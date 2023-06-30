eea05f437519454c3afdd356a02e7a3756c63868
Artem <nikolloteslla@gmail.com>
2023-06-30 15:20:12 +0300

    add active navigation

--- app/js/app.js
+++ app/js/app.js
@@ -1,63 +1,93 @@
 $(function () {
-  if ($('.header').length > 0){
-    const navOffset = $(".header").offset().top + 200;
-    $(window).scroll(function() {
-    
-      const scrolled = $(this).scrollTop();
-    
-      if (scrolled > navOffset){
-        $('header').addClass("sticky animate__animated animate__fadeInDown");
-        $('header').next().addClass("margin-top");
-      } else if (scrolled < navOffset){
-        $('header').removeClass("sticky animate__animated animate__fadeInDown");
-        $('header').next().removeClass("margin-top");
-      }
-    });
-  }
-  
-  $(".header__box button").on("click", function () {
-    $(".header__box button").removeClass("active");
-    $(this).addClass('active');
-  });
+	jQuery(window).scroll(function () {
+		var $sections = $('section');
+		$sections.each(function (i, el) {
+			var top = $(el).offset().top - 100;
+			var bottom = top + $(el).height();
+			var scroll = $(window).scrollTop();
+			var id = $(el).attr('id');
+			if (scroll > top && scroll < bottom) {
+				$('a.active').removeClass('active');
+				$('a[href="#' + id + '"]').addClass('active');
 
-  $(".header__menu-btn").on("click", function () {
-    $(".header__menu-btn").toggleClass("active");
-    $(".header__menu-wrapper").toggleClass("open");
-    $(".header").toggleClass("dark");
-    $("body").toggleClass("lock");
-  });
+			}
+		})
+	});
+
+	$("nav").on("click", "a", function (event) {
+		// исключаем стандартную реакцию браузера
+		event.preventDefault();
+
+		// получем идентификатор блока из атрибута href
+		var id = $(this).attr('href'),
+
+			// находим высоту, на которой расположен блок
+			top = $(id).offset().top;
+
+		// анимируем переход к блоку, время: 800 мс
+		$('body,html').animate({
+			scrollTop: top
+		}, 800);
+	});
+
+	const navOffset = $(".header").offset().top + 200;
+	$(window).scroll(function () {
+
+		const scrolled = $(this).scrollTop();
+
+		if (scrolled > navOffset) {
+			$('header').addClass("sticky animate__animated animate__fadeInDown");
+			$('header').next().addClass("margin-top");
+		} else if (scrolled < navOffset) {
+			$('header').removeClass("sticky animate__animated animate__fadeInDown");
+			$('header').next().removeClass("margin-top");
+		}
+	});
+
+	$(".header__box button").on("click", function () {
+		$(".header__box button").removeClass("active");
+		$(this).addClass('active');
+	});
+
+	$(".header__menu-btn").on("click", function () {
+		$(".header__menu-btn").toggleClass("active");
+		$(".header__menu-wrapper").toggleClass("open");
+		$(".header").toggleClass("dark");
+		$("body").toggleClass("lock");
+	});
 
 	var number = document.querySelector('.number'),
 		numberTop = number.getBoundingClientRect().top,
-		start = +number.innerHTML, end = +number.dataset.max;
+		start = +number.innerHTML,
+		end = +number.dataset.max;
 
-		window.addEventListener('scroll', function onScroll() {
-			if(window.pageYOffset > numberTop - window.innerHeight / 1.2) {
-				this.removeEventListener('scroll', onScroll);
-				var interval = setInterval(function() {
-					number.innerHTML = ++start;
-					if(start == end) {
-						clearInterval(interval);
-					}
-				}, 5);
-			}
-		});
+	window.addEventListener('scroll', function onScroll() {
+		if (window.pageYOffset > numberTop - window.innerHeight / 1.2) {
+			this.removeEventListener('scroll', onScroll);
+			var interval = setInterval(function () {
+				number.innerHTML = ++start;
+				if (start == end) {
+					clearInterval(interval);
+				}
+			}, 5);
+		}
+	});
 
-    var number1 = document.querySelector('.number1'),
+	var number1 = document.querySelector('.number1'),
 		number1Top = number1.getBoundingClientRect().top,
-		start1 = +number1.innerHTML, end1 = +number1.dataset.max;
+		start1 = +number1.innerHTML,
+		end1 = +number1.dataset.max;
 
-		window.addEventListener('scroll', function onScroll() {
-			if(window.pageYOffset > number1Top - window.innerHeight / 1.2) {
-				this.removeEventListener('scroll', onScroll);
-				var interval1 = setInterval(function() {
-					number1.innerHTML = ++start1;
-					if(start1 == end1) {
-						clearInterval(interval1);
-					}
-				}, 5);
-			}
-		});
+	window.addEventListener('scroll', function onScroll() {
+		if (window.pageYOffset > number1Top - window.innerHeight / 1.2) {
+			this.removeEventListener('scroll', onScroll);
+			var interval1 = setInterval(function () {
+				number1.innerHTML = ++start1;
+				if (start1 == end1) {
+					clearInterval(interval1);
+				}
+			}, 5);
+		}
+	});
 
 });
-
