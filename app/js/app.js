bc840d5e3e43e206bafc6b5a23dd651cf205960d
Artem <nikolloteslla@gmail.com>
2023-06-30 11:31:06 +0300

    review

--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,14 @@ $(function () {
     $(this).addClass('active');
   });
 
+  $(".header__menu-btn").on("click", function () {
+    $(".header__menu-btn").toggleClass("active");
+    $(".header__menu-wrapper").toggleClass("open");
+    // $(".header__list").toggleClass("active");
+    // $(".header--transparent").addClass("active");
+
+    $("body").toggleClass("lock");
+  });
+
 
 });
